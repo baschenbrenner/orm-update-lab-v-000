93e4b2fb1fe6d3f27c4638bcccfa9c38efba5302
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-29 12:35:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,11 @@ attr_accessor :id, :name, :grade
       
     end
 
+    def self.find_by_name(name)
+      
+      
+    end
+    
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
