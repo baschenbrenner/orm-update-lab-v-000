15119871562f4fcf4e2fd6f179fe06fc31b63571
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-29 12:35:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ attr_accessor :id, :name, :grade
       self.create(name, grade)
       
     end
-    
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
